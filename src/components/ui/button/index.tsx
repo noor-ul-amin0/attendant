69231400093c498081f125633749b9560a71f8ae
Noor Ul Amin <noorawan444@gmail.com>
2023-09-05 12:09:40 +0500

    Add reusable UI button component

---
+++ src/components/ui/button/index.tsx
@@ -0,0 +1,31 @@
+import React, { ButtonHTMLAttributes, CSSProperties } from "react";
+import "./styles.css";
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  style?: CSSProperties; // Dynamic styles
+  label: string; // Button label
+  onClick?: () => void; // Function to be called on click
+  disabled?: boolean; // Disable button
+}
+
+const Button: React.FC<ButtonProps> = ({
+  style,
+  label,
+  disabled = false,
+  onClick,
+  ...restProps
+}) => {
+  return (
+    <button
+      disabled={disabled}
+      onClick={onClick}
+      className="btn"
+      style={{ ...style }}
+      {...restProps}
+    >
+      {label}
+    </button>
+  );
+};
+
+export default Button;
